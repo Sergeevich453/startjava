dfed8d0e780b4c1574ba4fc6f6bb02dd01d1379a
ILYA EVDOKIMOV <miniservis@inbox.ru>
2022-07-02 23:51:11 +0300

    HomeWork 2

---
+++ Homework/HW1.java
@@ -0,0 +1,10 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        String name = "Irina";
+        int id = 1;
+        int age = 40;
+        double size = 56;
+        double experience = 15.2;
+            System.out.println(""Worker: " + " ID: " + id + " Name:"  name + " age:  " + age + " size: " + size + " experience: " + experience  ");
+    }
+}
---
+++ Homework/HomeWork1.java
@@ -0,0 +1,11 @@
+public class HomeWork1 {
+    public static void main(String[] args) {
+        String name = "Irina";
+        int id = 1;
+        int age = 40;
+        double size = 56;
+        double experience = 15.2;
+            System.out.println("Worker: " + "\t"+"\n" +" ID: " + id +"\n" + " Name: " + name +"\n" + " age: " + age +"\n" + " size: " + size +"\n" + " experience: " + experience);
+            System.out.println("");
+    }
+}
