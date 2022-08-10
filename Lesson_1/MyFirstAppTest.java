81614d7ac1a46af0e36341e82300243ea9ae9fbd
ILYA EVDOKIMOV <miniservis@inbox.ru>
2022-08-10 11:17:06 +0300

    implement HW 10_08_22

---
+++ Lesson_1/MyFirstAppTest.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class MyFirstAppTest{
+    public static void main(String [] args){
+        System.out.println("Hello World");
+        Scanner console = new Scanner(System.in, "cp866");
+        System.out.println("Введите свое имя: ");
+        System.out.println();
+        System.out.println(console.nextLine());
+
+    }       
+}
