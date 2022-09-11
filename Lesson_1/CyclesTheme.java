2f6abc7c37b8cee2ee2824f6c479b8ad222262c2
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-09-11 23:41:53 +0300

    HW_SVOY_6_10

--- Lesson_1/CyclesTheme.java
+++ Lesson_1/CyclesTheme.java
@@ -106,7 +106,7 @@ public class CyclesTheme {
 
         System.out.println();
         countSymbols = 5;
-        countLines = 0;
+        int countLines = 0;
         do {
             if (countSymbols < 3) {
                 countLines = countSymbols;
@@ -180,12 +180,23 @@ public class CyclesTheme {
         System.out.println("\n\t" + "ЗАДАНИЕ 10 \nВывод таблицы умножения Пифагора\n");
         System.out.print("\tТАБЛИЦА ПИФАГОРА " + "\n   ");
         for (int i = 2; i <= 9; i++) {
-            System.out.printf("%3d", i);
+            if(i == 2) {
+                System.out.printf("%1s%3d", (char) 124, i);
+            } else {
+                System.out.printf("%3d", i);
+            }
         }
         System.out.println();
         for (int i = 2; i < 10; i++) {
+            if(i == 2) {
+                System.out.println("  --------------------------");
+            }
             for(int j = 1; j < 10; j++) {
+                if(j == 1) {
+                System.out.printf("%3d%1s", i * j, (char) 124);
+            } else {
                 System.out.printf("%3d", i * j);
+            }
             }
             System.out.println();
         }
