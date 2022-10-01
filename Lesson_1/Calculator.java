8cc8f309cc9f79b5c2d0f93668b5dbba1d9ab86c
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-10-01 21:14:01 +0300

    HW_SVOY_6_15

---
+++ Lesson_1/Calculator.java
@@ -0,0 +1,24 @@
+public class CalculatorTest {
+    public static void main(String [] args){
+        int num1 = 10;
+        int num2 = 5;
+        int result = 1;
+        char sign = '+';
+        if (sign == '+') {
+            result = num1 + num2;
+        } else if (sign == '-') {
+            result = num1 - num2;
+        } else if (sign == '*') {
+            result = num1 * num2;
+        } else if (sign == '/') {
+            result = num1 / num2;
+        } else if (sign == '^') {
+            for (int i = 1; i <= num2; i++) {
+                result *= num1;
+            }
+        } else if (sign == '%') {
+            result = num1 % num2;
+        }
+        System.out.println(num1 + " " + sign + " " + num2 + " = " + result);
+    }
+}
