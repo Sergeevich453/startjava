a24fa7972c786e5cb9cfcffd5a461363fc54d0b6
EVDOKIMOV ILYA HOME <miniservis@inbox.ru>
2022-10-08 13:43:21 +0300

    HW_SVOY_6_17

---
+++ Lesson_1/Person.java
@@ -0,0 +1,32 @@
+public class Person {
+    
+    String gender = "мужской";
+    String name = "Илья";
+    int height = 182;
+    int weight = 83;
+    int age = 39;
+
+    void walk() {
+        System.out.println("Топ - топ!");
+    }
+
+    void sit() {
+        System.out.println("Я сел!");
+    }
+
+    void run() {
+        System.out.println("Я бегу!");
+    }
+
+    void sit() {
+        System.out.println("Я сел!");
+    }
+
+    String speak() {
+        return "Я говорю";
+    }
+
+    boolean studyJava() {
+        return true;
+    }
+}
