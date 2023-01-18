7df43ee532a289f1d5dcb6fa94202004e403b8ba
Ilya Buk <miniservis@inbox.ru>
2023-01-18 16:40:31 +0300

    New commit Buk 1

--- Lesson_1/MyFirstGame.java
+++ Lesson_1/MyFirstGame.java
@@ -1,6 +1,6 @@
 public class MyFirstGame {
     public static void main(String [] args){
-        int computerNum = 1;
+        int computerNum = 99;
         int playerNum = 100;
         int count = 0;
         while (playerNum != computerNum) {
@@ -9,7 +9,7 @@ public class MyFirstGame {
                 playerNum = playerNum / 2;
             } else if (playerNum < computerNum) {
                 System.out.println("Загаданное число больше = " + playerNum + " < " + computerNum); 
-                playerNum = playerNum + 1;
+                playerNum++;
             }
             count++;
         }
