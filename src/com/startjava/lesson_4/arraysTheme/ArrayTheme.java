81ee3b363c63d15ec859abbd6d49fd4d9d2867b3
Ilya Test <miniservis@inbox.ru>
2024-02-14 22:07:35 +0300

    H E

---
+++ src/com/startjava/lesson_4/arraysTheme/ArrayTheme.java
@@ -0,0 +1,9 @@
+package com.startjava.lesson_4.arraysTheme;
+
+public class ArrayTheme {
+
+	public static void main(String[] args) {
+		System.out.println("\n\t" + "ЗАДАНИЕ 1 \nРеверс значений массива\n");
+	}
+
+}
